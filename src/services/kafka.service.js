bf809360c4cfcd2dda1d8c83225762e10b0d4fe0
Yolotzin Groth <111380848+yoloogroth@users.noreply.github.com>
2023-05-22 10:53:08 -0600

    Update kafka.service.js

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -1,24 +1,24 @@
 
 class KafkaService {
-  url = 'https://kafka-express-service-yoloogroth.cloud.okteto.net/';
-
+   //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
+   url = 'https://kafka-express-service-yoloogroth.cloud.okteto.net';
+  
+   reaction = async (name) => {
+    await fetch(this.url + 'like?name=' + name, {
+       method: 'GET',
+       headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+       },  
+    })  
+       .then((response) => console.log(response.json()))
+       .then((data) => {
+         console.log(data);
+       })  
+       .catch((err) => {
+          console.log(err.message);
+       }); 
+   }
  
-  reaction = async (name) => {
-   await fetch(this.url + 'like?name=' + name, {
-      method: 'GET',
-      headers: {
-         'Content-type': 'application/json; charset=UTF-8',
-      },  
-   })  
-      .then((response) => console.log(response.json()))
-      .then((data) => {
-        console.log(data);
-      })  
-      .catch((err) => {
-         console.log(err.message);
-      }); 
-  }
-
-}
-
-export default new KafkaService();
+ }
+ // eslint-disable-next-line
+ export default new KafkaService();
