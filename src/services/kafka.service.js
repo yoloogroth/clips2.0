b7480606c28f60583a120447cc2b4d109a959b9a
Yolotzin Groth <111380848+yoloogroth@users.noreply.github.com>
2023-06-01 09:59:08 -0600

    Update kafka.service.js

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -3,22 +3,38 @@ class KafkaService {
    //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
    url = 'https://kafka-express-service-yoloogroth.cloud.okteto.net/';
   
-   reaction = async (name) => {
-    await fetch(this.url + 'like?name=' + name, {
-       method: 'GET',
-       headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-       },  
-    })  
-       .then((response) => console.log(response.json()))
-       .then((data) => {
-         console.log(data);
-       })  
-       .catch((err) => {
-          console.log(err.message);
-       }); 
-   }
- 
- }
- // eslint-disable-next-line
- export default new KafkaService();
+   reaction = async (uId, oId, rId) => {
+    await fetch(this.url + '/reaction?userId=' + uId+'&objectId='+oId +'&reactionId='+rId, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  comment = async (uId, oId, comment) => {
+    await fetch(this.url + '/comments?userId=' + uId+'&objectId='+oId +'&comment='+comment, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+}
+
+const kafkaService = new KafkaService();
+export default kafkaService;
